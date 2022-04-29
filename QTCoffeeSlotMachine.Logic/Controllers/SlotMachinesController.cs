62432a555b7b7e14db99f2e699e16425532e8c9e
Gehrer Gerhard <56598504+leoggehrer@users.noreply.github.com>
2022-04-29 10:02:34 +0200

    Updated to the newest template version.

--- QTCoffeeSlotMachine.Logic/Controllers/SlotMachinesController.cs
+++ QTCoffeeSlotMachine.Logic/Controllers/SlotMachinesController.cs
@@ -12,56 +12,35 @@ namespace QTCoffeeSlotMachine.Logic.Controllers
         public SlotMachinesController(ControllerObject other) : base(other)
         {
         }
-
-        public override Task<SlotMachine> InsertAsync(SlotMachine entity)
+        protected override void ValidateEntity(ActionType actionType, SlotMachine entity)
         {
             CheckEntity(entity);
 
-            return base.InsertAsync(entity);
-        }
-        public override Task<IEnumerable<SlotMachine>> InsertAsync(IEnumerable<SlotMachine> entities)
-        {
-            entities.ToList().ForEach(e => CheckEntity(e));
-
-            return base.InsertAsync(entities);
-        }
-
-        public override Task<SlotMachine> UpdateAsync(SlotMachine entity)
-        {
-            CheckEntity(entity);
-
-            return base.UpdateAsync(entity);
-        }
-
-        public override Task<IEnumerable<SlotMachine>> UpdateAsync(IEnumerable<SlotMachine> entities)
-        {
-            entities.ToList().ForEach(e => CheckEntity(e));
-
-            return base.UpdateAsync(entities);
+            base.ValidateEntity(actionType, entity);
         }
 
         private void CheckEntity(SlotMachine entity)
         {
             if (entity.Price < 0)
-                throw new Modules.Exceptions.LogicException($"....");
+                throw new Modules.Exceptions.LogicException($"Invalide value {entity.Price} for {nameof(entity.Price)}");
 
             if (entity.DepoteCoin5 < 0)
-                throw new Modules.Exceptions.LogicException($"....");
+                throw new Modules.Exceptions.LogicException($"Invalide value {entity.DepoteCoin5} for {nameof(entity.DepoteCoin5)}");
 
             if (entity.DepoteCoin10 < 0)
-                throw new Modules.Exceptions.LogicException($"....");
+                throw new Modules.Exceptions.LogicException($"Invalide value {entity.DepoteCoin10} for {nameof(entity.DepoteCoin10)}");
 
             if (entity.DepoteCoin20 < 0)
-                throw new Modules.Exceptions.LogicException($"....");
+                throw new Modules.Exceptions.LogicException($"Invalide value {entity.DepoteCoin20} for {nameof(entity.DepoteCoin20)}");
 
             if (entity.DepoteCoin50 < 0)
-                throw new Modules.Exceptions.LogicException($"....");
+                throw new Modules.Exceptions.LogicException($"Invalide value {entity.DepoteCoin50} for {nameof(entity.DepoteCoin50)}");
 
             if (entity.DepoteCoin100 < 0)
-                throw new Modules.Exceptions.LogicException($"....");
+                throw new Modules.Exceptions.LogicException($"Invalide value {entity.DepoteCoin100} for {nameof(entity.DepoteCoin100)}");
 
             if (entity.DepoteCoin200 < 0)
-                throw new Modules.Exceptions.LogicException($"....");
+                throw new Modules.Exceptions.LogicException($"Invalide value {entity.DepoteCoin200} for {nameof(entity.DepoteCoin200)}");
         }
     }
 }
